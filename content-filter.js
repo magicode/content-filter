aefa88fa0ca67258c2059e269d8f131ae836df81
magicode <magicode7@gmail.com>
2015-03-11 22:59:06 +0000

    add js & json

--- content-filter.js
+++ content-filter.js
@@ -9,32 +9,36 @@
 
 var entities = require("entities");
 var cheerio = require("cheerio");
-
+var acorn = require("acorn");
 
 module.exports = ContentFilter;
 
-function ContentFilter(list){
-    
-    var margin = '(?:\\b|[\\x20-\\x2f\\x3a-\\x40\\x5b-\\x60\\x7b-\\x7e])';
-	this.regex = new RegExp(margin + '(' + list.join('|') +  ')' + margin,'i');
-	
+function ContentFilter(options){
+	this.regex = options.regex;
 }
 
 ContentFilter.prototype.html = function(text){
-    this.regex;
+    
     var $this = this;
     $ = cheerio.load(text,{decodeEntities: false});
-  
+    
+    
+    var importantText = ' ';
     //title 
-    $('title').text();
+    importantText += $('title').text() + ' ';
     //<meta name="description"
-    $('meta[name=description]').attr('content');
+    importantText += $('meta[name=description]').attr('content') + ' ';
     //<meta name="keywords"
-    $('meta[name=keywords]').attr('content');
+    importantText += $('meta[name=keywords]').attr('content') + ' ';
+    
+    
+    if($this.regex.test(importantText)){
+        return { important: true };
+    }
     
     var listtext = [];
 
-    $('div,li,a,p,body').each(function(i,elm){
+    $('li,div,a,p,body').each(function(i,elm){
         var text = $(elm).text().trim();
         if(text.length)
             listtext.push({ length: text.length  , elm: elm });
@@ -57,5 +61,76 @@ ContentFilter.prototype.html = function(text){
         sumall += text.length;
     });
     
-    return { text: $.html({decodeEntities: false}) , remove: sumbad ,length: sumall }   ;
+    return { html: $.html({decodeEntities: false}) , remove: sumbad ,length: sumall }   ;
 };
+
+ContentFilter.prototype.js = function(code){
+    var $this =  this;
+    var outCode = '';
+    var tokens = [];
+    try{
+        acorn.parse(code, { ranges: true,  onToken: tokens });
+    }catch(e){
+        return { error: e };
+        
+    }
+    var startPoint = 0;
+    var removeCount = 0;
+    var length = 0;
+    tokens.forEach(function(token){
+        if(token.type  && token.type.type == 'string'){
+            length += token.value.length;
+            if($this.regex && $this.regex.test(' ' + token.value + ' ')){
+                
+                removeCount += token.value.length;
+                
+                outCode += code.substring(startPoint, token.start) + "''" ;
+                startPoint = token.end;
+            }
+        }
+    });
+    
+    outCode += code.substring(startPoint);
+    
+    return { js: outCode , remove: removeCount , length: length }; 
+};
+
+ContentFilter.prototype.json = function(json){
+    
+    var $this = this;
+    
+    var remove = 0;
+    var length = 0;
+    
+    function stringReplace(all ,q, string){
+
+        if(string){
+            
+            string = string.replace(/\\x([a-fA-F0-9]{2})/ig, function(a,b){
+                return String.fromCharCode(parseInt(b,16));
+            });
+    
+            string = string.replace(/\\u([a-fA-F0-9]{4})/ig, function(a,b){
+                return String.fromCharCode(parseInt(b,16));
+            });
+            
+            length += string.length;
+            if($this.regex && $this.regex.test(' ' + string + ' ')){
+                remove += string.length;
+                return q + q; 
+            }
+        }
+        return all;
+    }
+    
+    json = json.replace(/(")((\\"|[^"])*)"/g, stringReplace);
+    json = json.replace(/(')((\\'|[^'])*)'/g, stringReplace);
+    
+    return { json: json , remove: remove , length: length };
+};
+
+
+ContentFilter.prototype.text = function(text){
+    
+};
+
